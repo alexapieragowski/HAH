06aededf3673cd58dcd820381bf46472643d2c3d
haleyheshelman <heshelhj@rose-hulman.edu>
2015-05-08 08:58:44 -0400

    Small tweaks

--- src/Enemies.java
+++ src/Enemies.java
@@ -1,4 +1,20 @@
+import java.awt.Color;
 
-public class Enemies {
 
+public class Enemies extends Entity {
+	
+	public boolean toggle;
+
+	public Enemies(Color color, Integer pointValue, DiggerMain dm,
+			int x_position, int y_position) {
+		super(color, pointValue, dm, x_position, y_position);
+		toggle = true;
+	}
+	
+	public void enemySwitch(){
+		toggle = !toggle;
+	}
+	
+	
+	
 }
