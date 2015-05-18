f4de3d2a4e12a681046524abfc9e1e7db8fa960f
alexapieragowski <pieragab@rose-hulman.edu>
2015-05-18 17:15:54 -0400

    Edited gold classes

--- src/FallingGold.java
+++ src/FallingGold.java
@@ -2,9 +2,17 @@ import java.awt.Color;
 
 
 public class FallingGold extends Entity {
+	private int originalPosition;
 
 	public FallingGold(DiggerMain dm, int x_position, int y_position) {
 		super(Color.cyan, 0, dm, x_position, y_position, "FallingGold");
+		originalPosition = position[1];
+	}
+	
+	public void ifBreak(){
+		if (position[1]> originalPosition+2) {breakOpen();}
+		else {Gold newGold = new Gold(dm, position[0], position[1]);
+		level.entities[position[0]/level.imageSize][position[1]/level.imageSize] = newGold;}
 	}
 	
 	public void breakOpen(){
--- src/Gold.java
+++ src/Gold.java
@@ -5,7 +5,7 @@ public class Gold extends Entity {
 	private boolean canFall = true;
 	private int originalPosition[] = this.position;
 
-	public Gold(DiggerMain dm, int x_position, int y_position, String spriteName) {
+	public Gold(DiggerMain dm, int x_position, int y_position) {
 		super(Color.yellow, 0, dm, x_position, y_position, "Gold");
 	}
 	
@@ -17,7 +17,8 @@ public class Gold extends Entity {
 	}
 	
 	public void fallDown(){
-		if (level.entities[position[0]/level.imageSize][position[1]/level.imageSize+1].spriteName=="Empty"){
+		String ex = level.entities[position[0]/level.imageSize][position[1]/level.imageSize+1].spriteName;
+		if (!(ex.equals("Dirt")||ex.equals("Emerald"))){
 			//movement(0, level.imageSize);
 			FallingGold fallingGold = new FallingGold(dm, position[0], position[1]);
 			level.entities[position[0]/level.imageSize][position[1]/level.imageSize] = fallingGold;
